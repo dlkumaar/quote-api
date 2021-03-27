5f78c765e07ce378592aeac97427e6a264ed5291
Rahul Kumar <dev.rkumaar@gmail.com>
2021-03-26 22:18:39 -0300

    first commit: Quote API

---
+++ README.md
@@ -0,0 +1,5 @@
+## Quote API
+
+First backend project while studying at Codecademy.
+
+"Quote API" allows users to fetch a random quote, fetch all quotes listed in the database, fetch quotes by author, and post new quotes.
---
+++ backend/app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const morgan = require('morgan');
+
+// require local modules
+const { quotes } = require('./data');
+const { getRandomElement } = require('./utils');
+
+// App setup
+const app = express();
+
+/**
+ * MIDDLEWARES
+ */
+
+// Morgan
+app.use(morgan('dev'));
+
+// static pages
+
+app.use(express.static('public'));
+
+/**
+ *  ROUTES
+ */
+// random quote
+app.get('/api/quotes/random', (req, res) => {
+	const randomQuote = getRandomElement(quotes);
+	res.send({
+		quote: [randomQuote],
+	});
+});
+
+// all quotes array and filter by name
+app.get('/api/quotes', (req, res) => {
+	if (req.query.person !== undefined) {
+		const quotesByPerson = quotes.filter(
+			(quote) => quote.person === req.query.person
+		);
+		res.send({
+			quotes: quotesByPerson,
+		});
+	} else {
+		res.send({
+			quotes: quotes,
+		});
+	}
+});
+
+// post a new quote
+app.post('/api/quotes', (req, res) => {
+	const quote = req.query.quote;
+	const quoteBy = req.query.person;
+
+	if (!quote && !quoteBy) {
+		res.status(404).send('Missing data');
+	} else {
+		const newQuote = {
+			quote: quote,
+			person: quoteBy,
+		};
+
+		quotes.push(newQuote);
+		res.send({ quote: newQuote });
+	}
+});
+
+/**
+ * SERVER LISTENING AT PORT 5000
+ */
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server listenting at PORT: ${PORT}`));
